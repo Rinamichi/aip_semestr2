25f10b0123249125baf5a681cf9745730d0ff607
Rinamichi <nastjla24@mail.ru>
2021-02-27 00:18:11 +0300

    Task_4

---
+++ Task_4/Task_4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "cmath"
+
+int main()
+{
+    float x = -2;
+
+    std::cout << "Evaluate the expression: " << std::endl;
+    std::cout << fabs(x) + pow(x, 5) << std::endl;
+    return 0;
+
+    std::getchar();
+    std::getchar();
+}
+
+
