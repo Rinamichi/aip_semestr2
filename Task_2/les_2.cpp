a4f2ef702c9dcf5006b846db269718c003871b35
Rinamichi <nastjla24@mail.ru>
2021-02-27 00:11:52 +0300

    Task_2

---
+++ Task_2/les_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "cmath"
+
+int main()
+{
+    float a, b, c, S, P;
+    float Pi = 3.14;
+
+    std::cin >> a >> b;
+    S = Pi * a * b;
+    std::cout << "Square of ellipse = " << S << std::endl;
+    P = 4 * (((Pi * a * b) + ((a - b) * (a - b))) / (a + b));
+    std::cout << "Perimetre of ellipse = " << P;
+    return 0;
+
+    std::getchar();
+    std::getchar();
+}
+
