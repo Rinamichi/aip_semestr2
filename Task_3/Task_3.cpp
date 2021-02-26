8307226f51a0a860fea7697a5a72c6cdfbffbc67
Rinamichi <nastjla24@mail.ru>
2021-02-27 00:16:23 +0300

    Task_3

---
+++ Task_3/Task_3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "cmath"
+
+int main()
+{
+    float a;
+
+    std::cin >> a;
+    std::cout << "Square of number a = " << pow(a, 2) << std::endl;
+    std::cout << "Cube of number a = " << pow(a, 3) << std::endl;
+    return 0;
+
+    std::getchar();
+    std::getchar();
+}
+
+
