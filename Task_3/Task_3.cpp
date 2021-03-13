1291210a84dfe01c9b244d81c4045a4d6696779e
Rinamichi <nastjla24@mail.ru>
2021-03-13 22:15:41 +0300

    Task_3

--- Task_3/Task_3.cpp
+++ Task_3/Task_3.cpp
@@ -1,14 +1,21 @@
 ﻿#include <iostream>
 #include "cmath"
+#include "fstream"
 
 int main()
 {
+
+    std::ofstream f;
+    f.open("Task_3.txt");
+
     float a;
 
     std::cin >> a;
-    std::cout << "Square of number a = " << pow(a, 2) << std::endl;
-    std::cout << "Cube of number a = " << pow(a, 3) << std::endl;
+    f << "Square of number a = " << pow(a, 2) << std::endl;
+    f << "Cube of number a = " << pow(a, 3) << std::endl;
     return 0;
+
+    f.close();
 
     std::getchar();
     std::getchar();
