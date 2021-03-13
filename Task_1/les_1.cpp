a85272e4d6684b39f9a0dde7deef277e2a857ce5
Rinamichi <nastjla24@mail.ru>
2021-03-13 21:37:00 +0300

    Task_1

--- Task_1/les_1.cpp
+++ Task_1/les_1.cpp
@@ -1,19 +1,41 @@
 ﻿#include <iostream>
+#include "fstream"
 
 int main()
 {
-    std::cout << "    ***  " << std::endl
-              << "   *  ** " << std::endl
-              << "      ** " << std::endl
-              << "     * * " << std::endl
-              << "     * * " << std::endl
-              << "   **  * " << std::endl
-              << "   **  * " << std::endl
-              << "   *   * " << std::endl
-              << "  ****** " << std::endl
-              << "  *    * " << std::endl
-              << "  *    * " << std::endl
-              << "**     **" << std::endl;
+    //std::cout << "    ***  " << std::endl
+    //          << "   *  ** " << std::endl
+    //          << "      ** " << std::endl
+    //          << "     * * " << std::endl
+    //          << "     * * " << std::endl
+    //          << "   **  * " << std::endl
+    //          << "   **  * " << std::endl
+    //          << "   *   * " << std::endl
+    //          << "  ****** " << std::endl
+    //          << "  *    * " << std::endl
+    //          << "  *    * " << std::endl
+    //          << "**     **" << std::endl;
+
+
+        std::ofstream outFile;
+        outFile.open("Task_1.txt");
+
+    outFile << "    ***  " << std::endl
+        << "   *  ** " << std::endl
+        << "      ** " << std::endl
+        << "     * * " << std::endl
+        << "     * * " << std::endl
+        << "   **  * " << std::endl
+        << "   **  * " << std::endl
+        << "   *   * " << std::endl
+        << "  ****** " << std::endl
+        << "  *    * " << std::endl
+        << "  *    * " << std::endl
+        << "**     **" << std::endl;
+
+        outFile.close();
+
+
 
     std::getchar();
     std::getchar();
