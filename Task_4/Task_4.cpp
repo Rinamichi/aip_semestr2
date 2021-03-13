5e9896f4c51130c09684965f00ff1dbfcf4c5146
Rinamichi <nastjla24@mail.ru>
2021-03-13 22:22:54 +0300

    Task_4

--- Task_4/Task_4.cpp
+++ Task_4/Task_4.cpp
@@ -1,13 +1,20 @@
 ﻿#include <iostream>
 #include "cmath"
+#include "fstream"
 
 int main()
 {
+
+    std::ofstream f;
+    f.open("Task_4.txt");
+
     float x = -2;
 
-    std::cout << "Evaluate the expression: " << std::endl;
-    std::cout << fabs(x) + pow(x, 5) << std::endl;
+    f << "Evaluate the expression: " << std::endl;
+    f << fabs(x) + pow(x, 5) << std::endl;
     return 0;
+
+    f.close();
 
     std::getchar();
     std::getchar();
