05b316dcb2556122d80147264d699aef823635f7
Rinamichi <nastjla24@mail.ru>
2021-03-23 22:36:40 +0300

    Task_4

--- Task_4/Task_4.cpp
+++ Task_4/Task_4.cpp
@@ -1,23 +1,24 @@
-﻿#include <iostream>
+﻿/* Задание 4.
+Вычислите значение выражения: |x| + x^5 , если x=−2. Ответ: -30*/
+
+#include <iostream>
 #include "cmath"
 #include "fstream"
 
+using namespace std;
+
 int main()
 {
-
-    std::ofstream f;
+    ofstream f;
     f.open("Task_4.txt");
 
     float x = -2;
 
-    f << "Evaluate the expression: " << std::endl;
-    f << fabs(x) + pow(x, 5) << std::endl;
+    f << "Evaluate the expression: " << endl;
+    f << fabs(x) + pow(x, 5) << endl;
     return 0;
 
     f.close();
-
-    std::getchar();
-    std::getchar();
 }
 
 
