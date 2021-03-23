d540922c5fc375e73448929549f141ad0eef2932
Rinamichi <nastjla24@mail.ru>
2021-03-23 23:02:07 +0300

    Task_7

---
+++ Task_7/Task_7.cpp
@@ -0,0 +1,32 @@
+﻿/* Задание 7.
+Дана функция y=f(x). Найти значение функции по x: 
+y = 2x 2  + 10, если x > 5
+y = 0, если x = 0
+y = 2 * |x+6| - x 2 , если x < 5 */
+
+#include <iostream>
+#include "fstream"
+
+using namespace std;
+
+int main()
+{
+	ofstream f;
+	f.open("Task_7.txt");
+
+	float y, x;
+	cin >> x;
+	if (x > 5)
+		y = 2 * (x * x) +10;
+	else {
+		if (fabs(x) < 0.001)
+			y = 0;
+		else {
+			y = 2 * fabs(x + 6) - x * x;
+		}
+	}
+
+	f << "y = " << y;
+
+	f.close();
+}
