9ad5447bf9d6fcb5717f0ca8fb1f7a0e382335e8
Rinamichi <nastjla24@mail.ru>
2021-04-11 00:59:12 +0300

    Task_11

---
+++ Task_11/Task_11.cpp
@@ -0,0 +1,30 @@
+﻿/* Задание 11.
+Вывести на экран Н строк из нулей, причем количество нулей в каждой строке
+равно номеру строки. Количество строк Н вводит пользователь (можно из файла).
+*/
+
+#include <iostream>
+#include "fstream"
+
+using namespace std;
+
+float H;
+
+int main()
+{
+
+    ofstream f;
+    f.open("Task_11.txt");
+
+    cin >> H;
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < i; j++)
+            f << 0;
+        f << endl;
+    }
+
+    cout << endl;
+
+    f.close();
+}
+        
---
+++ Task_12/Task_12.cpp
@@ -0,0 +1,43 @@
+﻿/*Задание 12.
+Протабулировать функцию (шаг и диапазон задаёт пользователь):
+y = √2 + 3x  + 72x + tg(4x + 31)*/
+
+#include <iostream>
+#include <iomanip> 
+#include "cmath"
+#include "fstream"
+
+using namespace std;
+
+int main() {
+
+	ofstream fail;
+	fail.open("Task_12.txt");
+
+	float dx, x, df, f;
+
+	fail << "Table x and y. \ny = sqrt(2 + 3x)  + 72x + tg(4x + 31)" << endl;
+	fail << "Enter x: " << endl;
+	cin >> x;
+	fail << "Enter df:" << endl;
+	cin >> df;
+	fail << "Enter dx: " << endl;
+	cin >> dx;
+
+	fail << "\tx\t\ty" << endl;
+
+	fail.precision(3);
+
+	while (x < df) {
+		f = sqrt(2 + 3 * x) + 72 * x + tan(4 * x + 31);
+		fail << "\t" 
+			<< x
+			<< "\t\t"
+			<< f
+			<< endl;
+			 
+		x += dx;
+	}
+
+	return 0;
+}
