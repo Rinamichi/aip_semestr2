0da929fc5cea810baecadfdf7585b7ef34c93d35
Rinamichi <nastjla24@mail.ru>
2021-05-10 23:50:35 +0300

    Task_15

--- Task_15/Task_15.cpp
+++ Task_15/Task_15.cpp
@@ -6,39 +6,42 @@
 
 using namespace std;
 
-void func();
-float x, c, dy;
+struct parametr {
+	float x, c, dy;
+};
+
+void func(parametr A);
+
 int i = 7;
 int a = 2;
 float y = 0;
 
 int main()
 {
-	func();
+	parametr pA;
 
-	return 0;
-
+	func(pA);
 }
 
-void func(){
+void func(parametr A){
 
 	ofstream file;
 	file.open("Task_15.txt");
 
 	setlocale(LC_ALL, "Russian");
-	file << "Введите c(нечётное и больше 7): " << endl;
-	cin >> c;
-	file << "Введите x: " << endl;
-	cin >> x;
+	cin >> A.c;
+	file << "Введите c(нечётное и больше 7): " << A.c << endl;
+	cin >> A.x;
+	file << "Введите x: " << A.x << endl;
 	file << endl;
 
-	while (i < c) {
-		dy = (a + 1) / (i * x);
+	while (i < A.c) {
+		A.dy = (a + 1) / (i * A.x);
 		a = a + 1;
 		i = i + 2;
-		y = y + dy;
+		y = y + A.dy;
 
-		file << "dy = " << dy << endl
+		file << "dy = " << A.dy << endl
 			 << "i = " << i << endl
 			 << "a = " << a << endl;
 		file << endl;
