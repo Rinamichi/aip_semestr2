b2347fdfc0d73e8cc1539c4bf64833b5b7c380a2
Rinamichi <nastjla24@mail.ru>
2021-05-10 23:40:42 +0300

    Task_9

--- Task_9/Task_9.cpp
+++ Task_9/Task_9.cpp
@@ -7,24 +7,41 @@
 
 using namespace std;
 
+struct point {
+    float x, y;
+};
+
+const float r = 1;
+
+void area(point A);
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    point pA;
+
+    cin >> pA.x >> pA.y;
+    
+    area(pA);
+}
+
+void area(point A) {
+
     ofstream f;
     f.open("Task_9.txt");
+    
+    float rc = sqrt(A.x * A.x + A.y * A.y);
 
-    const float r = 1;
-    float x, y;
-    cin >> x >> y;
-    float rc = sqrt(x * x + y * y);
+    bool area1 = (rc < r) && (A.y > A.x) && (A.x > 0);
+    bool area2 = (rc < r) && (A.y > -A.x) && (A.x < 0);
 
-    bool area1 = (rc < r) && (y > x) && (x > 0);
-    bool area2 = (rc < r) && (y > -x) && (x < 0);
+    f << "Координата x: " << A.x << endl;
+    f << "Координата y: " << A.y << endl;
 
     if (area1 || area2)
-        f << "Yes";
+        f << "Да, точка попадает в заштрихованную область.";
     else
-        f << "No";
+        f << "Нет, точка не попадает в заштрихованную область.";
 
     f.close();
 }
-
