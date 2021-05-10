c706222ebb3d31faddfd99243656a4f7de73cd41
Rinamichi <nastjla24@mail.ru>
2021-05-10 23:34:28 +0300

    Task_6

--- Task_6/Task_6.cpp
+++ Task_6/Task_6.cpp
@@ -6,31 +6,64 @@
 
 using namespace std;
 
+struct number {
+	float a;
+};
+
+float b, s;
+
+float biggest(number x, number y, number z);
+
+float smallest(number x, number y, number z);
+
 int main()
 {	
-
+	setlocale(LC_ALL, "Russian");
 	ofstream f;
 	f.open("Task_6.txt");
 
-	float num, num2, num3;
+	number nx, ny, nz;
 
-	std::cin >> num >> num2 >> num3;
-	if ((num > num2) && (num > num3))
-		f << "So, num is the biggest then num + 5: " << num + 5 << endl;
-	if ((num2 > num) && (num2 > num3))
-		f << "So, num2 is the biggest then num2 + 5: " << num2 + 5 << endl;
-	if ((num3 > num) && (num3 > num2))
-		f << "So, num3 is the biggest then num3 + 5: " << num3 + 5 << endl;
-
-	if ((num < num2) && (num < num3))
-		f << "So, num is the smallest then num - 3: " << num - 3 << endl;
-	if ((num2 < num) && (num2 < num3))
-		f << "So, num2 is the smallest then num2 - 3: " << num2 - 3 << endl;
-	if ((num3 < num) && (num3 < num2))
-		f << "So, num3 is the smallest then num3 - 3: " << num3 - 3 << endl;
-	return 0;
+	cin >> nx.a >> ny.a >> nz.a;
+	
+	f << "Первое число = " << nx.a << endl;
+	f << "Второе число = " << ny.a << endl;
+	f << "Третье число = " << nz.a << endl;
+	f << "Отсюда biggest + 5 = " << biggest(nx, ny, nz) << endl;
+	f << "Отсюда smallest - 3 = " << smallest(nx, ny, nz) << endl;
 
 	f.close();
 }
 
+float biggest(number x, number y, number z) {
 
+
+	if ((x.a > y.a) && (x.a > z.a)) {
+		b = x.a + 5;
+	} else {
+		if ((y.a > x.a) && (y.a > z.a)) {
+			b = y.a + 5;
+		} else {
+			if ((z.a > x.a) && (z.a > y.a)) {
+				b = z.a + 5;
+			}
+		}
+	}
+	return (b);
+}
+
+float smallest(number x, number y, number z) {
+
+	if ((x.a < y.a) && (x.a < z.a)) {
+		s = x.a - 3;
+	} else { 
+		if ((y.a < x.a) && (y.a < z.a)) {
+			s = y.a - 3;
+		} else {
+			if ((z.a < x.a) && (z.a < y.a)) {
+				s = z.a - 3;
+			}
+		}
+	}
+	return (s);
+}
