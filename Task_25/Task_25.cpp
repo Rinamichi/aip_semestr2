e3b8290dbdaf7e7e7fd797d5f8939f6b17effdb6
Rinamichi <nastjla24@mail.ru>
2021-06-15 20:17:14 +0300

    Task_25

---
+++ Task_25/Task_25.cpp
@@ -0,0 +1,36 @@
+﻿/*Задание 25.
+ Увеличить все отрицательные элементы на 5, а положительные уменьшить на 1 */
+
+#include <iostream>
+#include "fstream"
+
+using namespace std;
+
+const int n = 20;
+int arrA[n];
+int num;
+
+int main()
+{
+	ofstream f;
+	f.open("Task_25.txt");
+	setlocale(LC_ALL, "Russian");
+
+	for (int i = 0; i < n; i++) {        //генерация массива со случайными элементами
+		arrA[i] = rand() % 20 - 10;
+
+		if (arrA[i] >= 0) {
+			num = arrA[i] - 1;
+		}
+		else {
+			num = arrA[i] + 5;
+		}
+
+		f << "Первоначальное значение масссива: "
+			<< arrA[i] << endl;
+		f << "Изменненое значение массива: "
+			<< num << endl;
+		f << endl;
+	}
+	f.close();
+}
