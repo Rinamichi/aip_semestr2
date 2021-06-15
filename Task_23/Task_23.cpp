193dfc75654bc588755583225d922cc366cb1477
Rinamichi <nastjla24@mail.ru>
2021-06-15 20:14:59 +0300

    Task_23

---
+++ Task_23/Task_23.cpp
@@ -0,0 +1,34 @@
+﻿/*Задание 23.
+ Найти номер минимального/максимального по модулю элемента. */
+
+#include <iostream>
+#include "fstream"
+
+using namespace std;
+
+int main()
+{
+	ofstream f;
+	f.open("Task_23.txt");
+
+	const int n = 20;
+	int arrA[n];
+	int res = 0;
+
+	for (int i = 0; i < n; i++) {        //генерация массива со случайными элементами
+		arrA[i] = rand() % 30-10;
+
+		if (abs(arrA[i]) > abs(arrA[i - 1])) {
+			res = i;
+		}
+	}
+
+	f << "Massive" << endl;
+	for (int i = 0; i < n; i++)
+		f << arrA[i] << endl;
+
+	f <<"Max: "<< res << endl;
+
+	f.close();
+}
+
